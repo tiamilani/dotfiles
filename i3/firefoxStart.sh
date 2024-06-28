c78a6ca65db5649e58e92a24bdf0300c233f6cbd
Mattia Milani <mattia.milani10@gmail.com>
2024-06-28 22:38:51 +0200

    dynamic monitor configuration

--- i3/firefoxStart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-firefox --class "Firefox" &
-firefox -P $social --class $social -new-window "https://web.telegram.org/k/" &
-firefox -P $social --class $social -new-window "https://teams.microsoft.com/?client=pwa" &
-firefox -P $social --class $social -new-window "https://outlook.office365.com/mail" &
-firefox -P $music --class $music -new-window "!yt" &
